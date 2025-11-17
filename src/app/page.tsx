5593a64418ec47ca873f16d2d5bfd68389c50558
qzda <qinzhidaoa@gmail.com>
2025-11-17 20:44:09 +0800

    feat: 清音

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
+import { 清音 } from "@/data";
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center font-default">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-5 px-10 sm:items-start">
-        <p className="text-xl">
-          人類社会のすべての構成員の固有の尊厳と平等で譲ることのできない権利とを承認することは、世界における自由、正義及び平和の基礎であるので、
-          人権の無視及び軽侮が、人類の良心を踏みにじった野蛮行為をもたらし、言論及び信仰の自由が受けられ、恐怖及び欠乏のない世界の到来が、一般の人々の最高の願望として宣言されたので、
-          人間が専制と圧迫とに対する最後の手段として反逆に訴えることがないようにするためには、法の支配によって人権を保護することが肝要であるので、
-          諸国間の友好関係の発展を促進することが肝要であるので、国際連合の諸国民は、国連憲章において、基本的人権、人間の尊厳及び価値並びに男女の同権についての信念を再確認し、かつ、一層大きな自由のうちで社会的進歩と生活水準の向上とを促進することを決意したので、
-        </p>
+    <div className="flex min-h-screen justify-center font-default">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center sm:items-start p-10 ">
+        <div className="text-xl w-full flex-auto flex flex-col gap-1">
+          {清音.map((row, rowIndex) => {
+            return (
+              <div
+                key={rowIndex}
+                className="w-full flex-auto flex justify-between gap-2"
+              >
+                {row.map((col, colIndex) => {
+                  return (
+                    <div key={colIndex}>
+                      {col ? `${col.hiragana} ${col.katakana}` : ""}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </main>
     </div>
   );
